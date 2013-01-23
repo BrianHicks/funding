004d53a5dd15af9e02f6a4a9aa599ee63892e272
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-22 20:34:20 -0600

    add user field

--- funding/apps/accounts/models.py
+++ funding/apps/accounts/models.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext as _
 from userena.models import UserenaBaseProfile
 
 
 class UserProfile(UserenaBaseProfile):
     'User Profile'
+    user = models.OneToOneField(
+        User,
+        unique=True,
+        verbose_name=_('user'),
+        related_name='profile',
+    )
