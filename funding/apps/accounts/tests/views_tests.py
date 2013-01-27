a4c3f27535e9391a506d793f4e082b265be76ca4
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-27 08:18:26 -0600

    fix bug by removing namespace

--- funding/apps/accounts/tests/views_tests.py
+++ funding/apps/accounts/tests/views_tests.py
@@ -18,11 +18,11 @@ class ReceiverAddAccountViewTests(TestCase):
 
     def test_returns_200(self):
         'returns 200 for authenticated user'
-        response = self.client.get(reverse('accounts:funding:add'))
+        response = self.client.get(reverse('funding:add'))
         self.assertEqual(200, response.status_code)
 
     def test_returns_404(self):
         'returns 404 for an unauthorized user'
         self.client.logout()
-        response = self.client.get(reverse('accounts:funding:add'))
+        response = self.client.get(reverse('funding:add'))
         self.assertEqual(302, response.status_code)
