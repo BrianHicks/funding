56d66b1d476b3f6a236356887a643ceecccef9b3
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-27 08:11:33 -0600

    add funding namespace and urls

--- funding/apps/accounts/urls.py
+++ funding/apps/accounts/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls.defaults import patterns, url, include
 
+from funding.apps.accounts import views
+
+accounts_urls = patterns(
+    '',
+    url(r'^add/$', views.ReceiverAddAccountView.as_view(), name='add')
+)
+
 urlpatterns = patterns(
     '',
+    url(r'^funding/', include(accounts_urls, namespace='funding')),
     url('', include('userena.urls')),
 )
