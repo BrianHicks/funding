23b228d4ce91a7522a8081b68d63aeba5820236c
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-31 07:09:41 -0600

    do actual validation

--- funding/assets/scripts/balanced/routing-number.js
+++ funding/assets/scripts/balanced/routing-number.js
@@ -2,9 +2,10 @@ function RoutingNumberModel(initial) {
     var self = this;
 
     self.value = ko.observable(initial || '');
-    self.valid = ko.observable(function() {
-        if (self.value() <= 5) return true;
-        return false;
+    self.valid = ko.computed(function() {
+        var value = self.value();
+        if (value.length <= 5) return true;
+        return balanced.bankAccount.validateRoutingNumber(value);
     });
     self.error = ko.observable('');
 }
