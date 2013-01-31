3e82948d1f12531ec884b13b6a6c34c272de1d1b
Brian Hicks <brian.hicks@rockfishinteractive.com>
2013-01-31 07:29:34 -0600

    fix check threshold

--- funding/assets/scripts/balanced/account-number.js
+++ funding/assets/scripts/balanced/account-number.js
@@ -2,18 +2,18 @@ function AccountNumberModel(initial) {
     var self = this;
 
     self.value = ko.observable(initial || '');
+    self.error = ko.observable('');
     self.valid = ko.computed(function() {
         var value = self.value();
-        if (value.length <= 5) return true;
+        if (value === '') return true;
 
         // balanced doesn't provide a helper method in this case, so we just
         // make sure it's all digits
         var valid = true;
-        if (/\D/.exec(value !== null)) {
+        if (/\D/.exec(value) !== null) {
             valid = false;
             self.error('account number should be all digits');
         } // else if... for more validations
         return valid
     });
-    self.error = ko.observable('');
 }
